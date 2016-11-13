fabbd8287e50b47d11db6d15593ddbddf447b231
Alexey Dobrynin <ag.dobrynin@gmail.com>
2016-11-13 23:36:40 +0400

    refactory tasl 3a

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ let pc = {};
 const RouteDiskVolumesName="volumes";
 const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
 
+fetch(pcUrl)
+  .then(async (res) => {
+    pc = await res.json();
+  })
+  .catch(err => {
+    console.log('Чтото пошло не так:', err);
+  });
+
 const app = express();
 app.use(cors());
 
@@ -15,58 +23,55 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// task 3a lesson 3
-app.get('/task3a/:p1?/:p2?/:p3?', (req, res) => {
+// task 3a, lesson 3 Route для подссчта свободного места
+app.get('/task3a/volumes', (req, res) => {
 
-  if( req.params.p1 == undefined ){
+  let myDisks = {};
 
-    res.json(pc);
-
-  } else if( pc[req.params.p1] !== undefined && req.params.p2 == undefined ){
-
-    res.json(pc[req.params.p1]);
-
-  } else if( pc[req.params.p1] !== undefined && pc[req.params.p1][req.params.p2] !== undefined ){
-
-    if( pc[req.params.p1][req.params.p2][req.params.p3] !== undefined ){
-
-      res.json(pc[req.params.p1][req.params.p2][req.params.p3]);
-
-    }else if(pc[req.params.p1][req.params.p2] !== undefined && typeof pc[req.params.p1][req.params.p2] !== 'function'){
-
-      res.json(pc[req.params.p1][req.params.p2]);
-
-    } else {
-
-        res.status(404).send('Not found');
-    }
-    
-  } else if( req.params.p1 == RouteDiskVolumesName ){
-
-    let myDisk={}, myDiskSize={};
-
-    for( let i=0; i< pc.hdd.length; i++ ) {
-      myDisk[pc.hdd[i].volume.toString()] = ( myDisk[pc.hdd[i].volume.toString()] == undefined ? 0 : myDisk[pc.hdd[i].volume.toString()] ) + pc.hdd[i].size;
-      myDiskSize[pc.hdd[i].volume.toString()] = myDisk[pc.hdd[i].volume.toString()] + 'B'
-    }
-
-    res.json(myDiskSize);
-
-  }else{
-
+  if(pc.hdd == undefined )
+  {
     res.status(404).send('Not found');
-
   }
 
+  for( let disk in pc.hdd ){
+    let size = pc.hdd[disk].size;
+    if (myDisks[pc.hdd[disk].volume]) {
+      size = parseInt(myDisks[pc.hdd[disk].volume], 10) + pc.hdd[disk].size;
+    }
+    myDisks[pc.hdd[disk].volume] = `${size}B`;
+  }
+
+  res.json(myDisks);
 });
 
-fetch(pcUrl)
-  .then(async (res) => {
-    pc = await res.json();
-  })
-  .catch(err => {
-    console.log('Чтото пошло не так:', err);
-  });
+// task 3a refactoring, lesson 3
+app.get('/task3a(*)?', (req, res) => {
+  let aParams, Params;
+  //массив параметров для поиска
+  if(req.params[0] !== undefined){
+    Params=req.params[0].trim().replace(/^\/|\/$/g,'');
+    aParams=Params==""?[]:Params.split('/');
+  }else{
+    aParams=[];
+  }
+
+  let pc_res = pc;
+
+  for( let i in aParams ){
+    if( pc_res[aParams[i]] !== undefined && ( aParams[i] !== 'length' ) ){
+      pc_res = pc_res[aParams[i]];
+    }else{
+      pc_res = undefined;
+      break;
+    }
+  }
+
+  if(pc_res == undefined && pc_res !== null)
+    res.status(404).send('Not Found');
+  else
+    res.json(pc_res);
+});
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000 ...');
